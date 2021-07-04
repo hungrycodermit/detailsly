71ee73881f0a0fe71e9391aa4aa666c5f26cc1d9
mitrao <mitrao@gmail.com>
2021-07-04 19:22:38 +0530

    final release v1

--- momo/settings.py
+++ momo/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ezhkbnavc%w)m&7lkdq)g41^yp7t(tu%7mw=8!sy)kkleiluo='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["detailsly.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'momo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'customer',
+#         'USER': 'root',
+#         'PASSWORD': 'adminadmin',
+#         'PORT': 3306,
+#         'HOST': '127.0.0.1',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'customer',
-        'USER': 'root',
-        'PASSWORD': 'adminadmin',
-        'PORT': 3306,
-        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
